332a1b0ecf1c1aaf6bc8f5f2edd680215b7b381d
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-10 15:39:58 -0600

    hide movie fields until after search complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 const apiKey = '4f7e92fe'
 
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
-  const [movie, setMovie] = useState({})
+  const [movie, setMovie] = useState(null)
+
 
   const updateSearchTerm = (event) => {
     setSearchTerm(event.target.value)
   }
+
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -22,6 +25,21 @@ function App() {
     .catch(() => console.log('error'))
   }
 
+  let movieDisplay = ""
+  if(movie) {
+    movieDisplay = (
+      <div>
+        <h2>Title: {movie.Title}</h2>
+        <h3>Year: {movie.Year}</h3>
+        <img src={movie.Poster} alt={movie.Title}/>
+        <h4>Genre: {movie.Genre}</h4>
+        <h5>Plot: {movie.Plot}</h5>
+      </div>
+    )
+  } else {
+    movieDisplay = ""
+  }
+
   return (
     <div className="App">
       <h1>OMDb MOVIE APP</h1>
@@ -30,49 +48,9 @@ function App() {
         <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="Movie Title"/>
         <input type="submit" value="Find Movie Info"/>
       </form>
-      <div>
-        <h2>Title: {movie.title}</h2>
-        <h3>Year: {movie.Year}</h3>
-        <img src={movie.Poster} alt={movie.Title}/>
-        <h4>Genre: {movie.Genre}</h4>
-        <h5>Plot: {movie.Plot}</h5>
-      </div>
+      {movieDisplay}
     </div>
   );
 }
 
 export default App;
-
-
-
-// import {useState} from 'react'
-// import './App.css';
-
-// const apiKey = "55b7d8f5"
-
-// function App() {
-//   const [searchTerm, setSearchTerm] = useState("")
-  
-//   const updateSearchTerm = (event) => {
-//     setSearchTerm(event.target.value)
-//   }
-//   const handleSubmit = (event) => {
-//     event.preventDefault()
-//     fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
-//       .then(response => response.json())
-//       .then(data => console.log(data))
-//       .catch(()=> console.log('error'))
-//   }
-//   return (
-//     <div className="App">
-//       <h1>OMDb MOVIE APP</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label>Title:</label>
-//         <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="Movie Title"/>
-//         <input type="submit" value="Find Movie Info" />
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default App
