0fac5a69f69a27d67d2ed0a5935bbcf986ff1107
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-10 14:43:14 -0600

    Bind balue of input to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+  const updateSearchTerm = (event) => {
+    setSearchTerm(event.target.value)
+  }
   return (
     <div className="App">
       <h1>OMDb MOVIE APP</h1>
+      <form>
+        <label>Title: </label>
+        <input on Change={updateSearchTerm} value={searchTerm} placeholder="Movie Title" />
+        <input type="submit" value="Find Movie Info"/>
+      </form>
     </div>
   );
 }
