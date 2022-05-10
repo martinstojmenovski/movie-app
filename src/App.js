0ec5674a89f83b6945a804041489c67cb44941f3
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-10 15:15:45 -0600

    Make request to API when form is submitted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import {useState} from 'react'
 import './App.css';
 
+const apiKey = '4f7e92fe'
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
+
   const updateSearchTerm = (event) => {
     setSearchTerm(event.target.value)
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+    .then (response => response.json())
+    .then(data => console.log(data))
+    .catch(() => console.log('error'))
+  }
+
   return (
     <div className="App">
       <h1>OMDb MOVIE APP</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Title: </label>
-        <input on Change={updateSearchTerm} value={searchTerm} placeholder="Movie Title" />
+        <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="Movie Title"/>
         <input type="submit" value="Find Movie Info"/>
       </form>
     </div>
@@ -19,3 +31,37 @@ function App() {
 }
 
 export default App;
+
+
+
+// import {useState} from 'react'
+// import './App.css';
+
+// const apiKey = "55b7d8f5"
+
+// function App() {
+//   const [searchTerm, setSearchTerm] = useState("")
+  
+//   const updateSearchTerm = (event) => {
+//     setSearchTerm(event.target.value)
+//   }
+//   const handleSubmit = (event) => {
+//     event.preventDefault()
+//     fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//       .catch(()=> console.log('error'))
+//   }
+//   return (
+//     <div className="App">
+//       <h1>OMDb MOVIE APP</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>Title:</label>
+//         <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="Movie Title"/>
+//         <input type="submit" value="Find Movie Info" />
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App
