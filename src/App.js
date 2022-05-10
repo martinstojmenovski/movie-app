5898895fb57a241336537b460d521fc13aed405e
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-05-10 16:00:46 -0600

    Handle if api does not find movie and add comments

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 import {useState} from 'react'
 import './App.css';
 
-const apiKey = '4f7e92fe'
+// api key to omdb api
+const apiKey = "55b7d8f5"
 
-
+// app component
 function App() {
+  // state for input search term
   const [searchTerm, setSearchTerm] = useState("")
+  // state for movie data from api
   const [movie, setMovie] = useState(null)
+  // state for message to user
+  const [message, setMessage] = useState("")
 
-
+  // update searchTerm in state based on input field value
   const updateSearchTerm = (event) => {
     setSearchTerm(event.target.value)
   }
-
-
+  // make ajax request for search term and display data
   const handleSubmit = (event) => {
+    // prevent page reload
     event.preventDefault()
+    // make ajax request for search term with api key
     fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
-    .then (response => response.json())
-    .then(data => {
-      setMovie(data)
-      setSearchTerm("")
-    })
-    .catch(() => console.log('error'))
+      // convert response to javascript object
+      .then(response => response.json())
+      // data from api as javascript object
+      .then(data => {
+        // reset searchTerm to empty
+        setSearchTerm('')
+        // if the API did not find a movie
+        if (data.Error) {
+          // display error message to user
+          setMessage(data.Error)
+          // reset movie to null
+          setMovie(null)
+        // otherwise api found movie
+        } else {
+          // set movie to api's movie data
+          setMovie(data)
+          // set message to empty
+          setMessage('')
+        }
+      })
+      // if api errors upon request
+      .catch(()=> setMessage('Error, sorry try again'))
   }
 
+  // build movie display jsx if there is a movie in state
   let movieDisplay = ""
-  if(movie) {
+  if (movie !== null) {
     movieDisplay = (
       <div>
         <h2>Title: {movie.Title}</h2>
@@ -36,18 +59,19 @@ function App() {
         <h5>Plot: {movie.Plot}</h5>
       </div>
     )
-  } else {
-    movieDisplay = ""
   }
 
+  // render form with handlesubmit and input with value bound to state
+  // display movie and message
   return (
     <div className="App">
       <h1>OMDb MOVIE APP</h1>
       <form onSubmit={handleSubmit}>
-        <label>Title: </label>
+        <label>Title:</label>
         <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="Movie Title"/>
-        <input type="submit" value="Find Movie Info"/>
+        <input type="submit" value="Find Movie Info" />
       </form>
+      <p>{message}</p>
       {movieDisplay}
     </div>
   );
